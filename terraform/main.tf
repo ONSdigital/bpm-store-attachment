01170fc347a7c7cd183f361aaa8bde871b7561ab
AlexWhitham <55181116+AlexWhitham@users.noreply.github.com>
2020-06-10 09:09:42 +0100

    Removed aws setup in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,4 @@
-provider "aws" {}
-
 locals  {
     prefix = "${var.app}-${var.stage}"
     attachments_bucket = "${terraform.workspace}-attachments-out.${var.domain}"
-}
-
-terraform {
-  backend "s3" {
-    bucket               = "terraform.bpm.ons.digital"
-    key                  = "store-attachment/lambda.tfstate"
-    workspace_key_prefix = "workspaces"
-  }
 }
