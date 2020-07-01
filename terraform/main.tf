e8370662c21f66cb2f400db475470e1394460ed5
AlexWhitham <55181116+AlexWhitham@users.noreply.github.com>
2020-07-01 11:56:21 +0100

    Changed the way bucket name is formed for flexibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
 locals {
   prefix = "${var.app}-${var.stage}"
-  attachments_bucket = "${terraform.workspace}-${var.app}-${var.stage}-out.${var.domain}"
+  attachments_bucket = "${terraform.workspace}-${var.app}-${var.stage}-attachments-out.${var.domain}"
 }
