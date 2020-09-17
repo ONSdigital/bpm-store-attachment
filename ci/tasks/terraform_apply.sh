a00818205077398014e22a6f749cbe23933cf98b
Phil Bambridge <phil.bambridge@ons.gov.uk>
2020-09-17 15:39:41 +0100

    Sanitise branch name

--- ci/tasks/terraform_apply.sh
+++ ci/tasks/terraform_apply.sh
@@ -4,7 +4,7 @@
 
 set -euo pipefail
 
-: ${WORKSPACE:=`jq -r '.[] | select(.name == "head_name").value' ./bpm-store-attachment/.git/resource/metadata.json`}
+: ${WORKSPACE:=`jq -r '.[] | select(.name == "head_name").value' ./bpm-store-attachment/.git/resource/metadata.json | sed 's/[^a-zA-Z0-9]/_/g'`}
 : ${TERRAFORM_SOURCE}
 : ${TF_VAR_stage}
 : ${AWS_REGION}
